a0a50f940572ece2785784c17063a4a76405f796
Nikita Rusanov <kenya.rusanov@gmail.com>
2020-10-05 11:40:16 +0300

    create dir for images

--- src/PDFPrint_3/converters/ConverterToJPEG.java
+++ src/PDFPrint_3/converters/ConverterToJPEG.java
@@ -33,32 +33,39 @@ public class ConverterToJPEG  implements ConverterPDF {
 
     public void convert()  {
         images = new ArrayList<>();
-        File destinationFile = new File(destination);
 
-        if (!destinationFile.exists()) {
-            destinationFile.mkdir();
-            System.out.println("DESTINATION FOLDER CREATED -> " + destinationFile.getAbsolutePath());
-        }
         if (inputPDF.exists()) {
             PDDocument doc;
             try {
                 doc = PDDocument.load(inputPDF);
 
+
+
                 PDFRenderer renderer = new PDFRenderer(doc);
                 String fileName = inputPDF.getName().replace(".pdf", "");
+
+                File destinationFile = new File(destination +  File.separator + fileName);
+
+                if (!destinationFile.exists()) {
+                    destinationFile.mkdir();
+                    System.out.println("DESTINATION FOLDER CREATED -> " + destinationFile.getAbsolutePath());
+                }
+
+
                 System.out.println("CONVERTER START.....");
                 for (int i = 0; i < doc.getNumberOfPages(); i++) {
                     BufferedImage image = renderer.renderImageWithDPI(i, 300);
-                    File fileTemp = new File(destination + fileName + "_" + i + ".jpg"); // jpg or png
+                    File fileTemp = new File(destinationFile ,   fileName + "_" + i + ".jpg"); // jpg or png
                     ImageIO.write(image, "JPEG", fileTemp); // JPEG or PNG
                     images.add(fileTemp);
                 }
                 doc.close();
+
+            System.out.println("CONVERTER STOPTED.....");
+            System.out.println("IMAGE SAVED AT -> " + destinationFile.getAbsolutePath());
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println("CONVERTER STOPTED.....");
-            System.out.println("IMAGE SAVED AT -> " + destinationFile.getAbsolutePath());
         } else {
             System.err.println(inputPDF.getName() + " FILE DOES NOT EXIST");
         }
